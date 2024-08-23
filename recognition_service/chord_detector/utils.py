65f58d46508482a6b988347bb4f265a90c3879bb
Ben Aidlin <ben.aidlin@intel.com>
2024-08-23 14:10:33 +0300

    update utils and requirements to use madmom

--- recognition_service/chord_detector/utils.py
+++ recognition_service/chord_detector/utils.py
@@ -1,29 +1,6 @@
-import librosa  
-import numpy as np  
-import tensorflow as tf  
+from madmom.audio.chord import DeepChromaChordRecognitionProcessor  
   
 def process_audio_file(file):  
-    y, sr = librosa.load(file, sr=None)  
-      
-    # Load your pretrained model  
-    model = tf.keras.models.load_model('path_to_your_model')  
-      
-    # Preprocess the audio for your model  
-    mel_spectrogram = librosa.feature.melspectrogram(y, sr=sr)  
-    mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)  
-      
-    # Make predictions  
-    predictions = model.predict(mel_spectrogram[np.newaxis, ..., np.newaxis])  
-      
-    # Assuming the model outputs a probability distribution for each chord at each time step  
-    chord_labels = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'N']  
-    timestamps = librosa.frames_to_time(np.arange(predictions.shape[1]), sr=sr)  
-      
-    chords = []  
-    for i, frame_predictions in enumerate(predictions[0]):  
-        chord_index = np.argmax(frame_predictions)  
-        chord = chord_labels[chord_index]  
-        timestamp = timestamps[i]  
-        chords.append((timestamp, chord))  
-      
-    return chords
+    processor = DeepChromaChordRecognitionProcessor()  
+    chords = processor(file)  
+    return [(timestamp, chord) for timestamp, chord in chords]  
